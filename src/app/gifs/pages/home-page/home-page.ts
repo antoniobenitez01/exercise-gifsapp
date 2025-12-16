cc3f41ce0484f27702fd393fa1da9f60b68263d4
antoniobenitez01 <anthonybenitezcontact@gmail.com>
2025-12-16 17:15:36 +0100

    Automatic Angular Push

--- src/app/gifs/pages/home-page/home-page.ts
+++ src/app/gifs/pages/home-page/home-page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../services/gifs';
+import { Gif } from '../../interfaces/gif-interface';
 
 @Component({
   selector: 'gifs-homepage',
@@ -7,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.css',
 })
 export class HomePageComponent {
+  constructor(private gifsService : GifsService ){}
 
+  get gifs() : Gif[]{
+    return this.gifsService.listadoGifs;
+  }
 }
